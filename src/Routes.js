010471f1631c41ae70c44af267630222a5a9931b
varunkumarnr <varunnrk@gmail.com>
2021-05-03 12:51:11 +0530

    editor

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Editor from "./pages/Editor";
 import { Home } from "./pages/Home";
 export const Routes = () => {
